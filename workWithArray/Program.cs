4f79b2492f2889bab9280e62c4673250a677ca5b
VitaliyBondar <ipz234_bvo@student.ztu.edu.ua>
2023-11-27 09:41:30 +0200

    У рішенні Sana03 у проєкті доданий масив, у якому користувач задає розмір та генерація псевдовипадкових чисел на проміжку,який задає користувач

---
+++ workWithArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
+
+        bool checkN, checkA, checkB;
+        short N;
+        int a, b;
+        Random rnd = new Random();
+
+        Console.WriteLine("Введіть розмір масиву(N):");
+        do
+        {
+            checkN = short.TryParse(Console.ReadLine(), out N);
+            if (!checkN || N < 0) Console.WriteLine("Значення N має бути більше нуля та бути числом ");
+        } while (!checkN || N < 0);
+
+        Console.WriteLine("Введіть розмір границі діапазону a - [a;b]");
+        do
+        {
+            checkA = int.TryParse(Console.ReadLine(), out a);
+            if (!checkA || a < 0) Console.WriteLine("Перевірте чи коректно задано значення a [a;b]");
+        } while (!checkA || a < 0);
+
+        Console.WriteLine("Введіть розмір границі діапазону b - [a;b]");
+        do
+        {
+            checkB = int.TryParse(Console.ReadLine(), out b);
+            if (!checkB || b < 0 || b < a) Console.WriteLine("Перевірте чи коректно задано значення b [a;b]");
+        } while (!checkA || b < 0 || b < a);
+
+        int[] array = new int[N];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(a, b + 1);
+            Console.WriteLine($"array{i} = {array[i]}");
+        }
+    }
+}
